d3e54cc521b194c922c291cbd7c21f9372def251
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2024-10-10 13:46:22 -0230

    Extended test suite

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,3 +1,7 @@
+import { describe, expect, test } from "@jest/globals";
+import "./toBeAnyOf";
+import "./toContainAnyOf";
+
 import {
   allRanks,
   filterRanksByCategory,
@@ -6,20 +10,70 @@ import {
   CAFElement,
 } from "../src/main";
 
-test.todo("test everything");
-
-describe("Test allRanks", () => {
-  test("can get category name", () => {
-    expect(allRanks[0].getCategoryName()).toEqual(
-      expect.objectContaining({
-        en: expect.any(String),
-        fr: expect.any(String),
-      })
-    );
+describe("test allRanks", () => {
+  test("has exactly 40 items", () => {
+    expect(allRanks).toHaveLength(40);
   });
 });
 
-describe("Test filterRanksByCategory()", () => {
+describe.each(allRanks)(
+  'test properties of rank "$title.en" in allRanks',
+  (rank) => {
+    test("level property is number in range 1-19", () => {
+      expect(rank).toHaveProperty("level");
+      expect(rank.level).toBeGreaterThanOrEqual(1);
+      expect(rank.level).toBeLessThanOrEqual(19);
+    });
+
+    test("element property contains a valid CAF element", () => {
+      expect(rank).toHaveProperty("element");
+      expect(rank.element).toContainAnyOf(["CA", "RCAF", "RCN"]);
+    });
+
+    test("title property is bilingual string", () => {
+      expect(rank).toHaveProperty("title");
+      expect(rank.title).toEqual(
+        expect.objectContaining({
+          en: expect.any(String),
+          fr: expect.any(String),
+        })
+      );
+    });
+
+    test("abbreviation property is bilingual string", () => {
+      expect(rank).toHaveProperty("abbreviation");
+      expect(rank.abbreviation).toEqual(
+        expect.objectContaining({
+          en: expect.any(String),
+          fr: expect.any(String),
+        })
+      );
+    });
+
+    test("category property is a valid category", () => {
+      expect(rank).toHaveProperty("category");
+      expect(rank.category).toBeAnyOf([
+        "FlagOfficer",
+        "SeniorOfficer",
+        "JuniorOfficer",
+        "SubordinateOfficer",
+        "SeniorNCO",
+        "JuniorNCM",
+      ]);
+    });
+
+    test("getCategoryName method returns bilingual string", () => {
+      expect(rank.getCategoryName()).toEqual(
+        expect.objectContaining({
+          en: expect.any(String),
+          fr: expect.any(String),
+        })
+      );
+    });
+  }
+);
+
+describe("test filterRanksByCategory()", () => {
   test.each([
     ["FlagOfficer", 8],
     ["SeniorOfficer", 6],
@@ -34,7 +88,7 @@ describe("Test filterRanksByCategory()", () => {
   });
 });
 
-describe("Test filterRanksByElement()", () => {
+describe("test filterRanksByElement()", () => {
   test.each([
     ["CA", 19],
     ["RCAF", 19],
