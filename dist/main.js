5a883a44136f197c7f3be5b04259638b0392f15c
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2024-10-15 10:00:41 -0230

    run build

--- dist/main.js
+++ dist/main.js
@@ -1,30 +1,27 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+import ranksJSON from "./caf-ranks.json";
+export const rankMeta = {
+    command: ranksJSON.command,
+    category: ranksJSON.category,
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.allRanks = exports.rankMeta = void 0;
-exports.filterRanksByElement = filterRanksByElement;
-exports.filterRanksByCategory = filterRanksByCategory;
-const caf_ranks_json_1 = __importDefault(require("./caf-ranks.json"));
-exports.rankMeta = {
-    command: caf_ranks_json_1.default.command,
-    category: caf_ranks_json_1.default.category,
-};
-exports.allRanks = caf_ranks_json_1.default.allRanks.map((rank) => (Object.assign(Object.assign({}, rank), { commands: rank.commands, category: rank.category, getCategoryName: function () {
-        return exports.rankMeta.category[this.category];
-    } })));
-function filterRanksByElement(ranks, command) {
+export const allRanks = ranksJSON.allRanks.map((rank) => ({
+    ...rank,
+    commands: rank.commands,
+    category: rank.category,
+    getCategoryName: function () {
+        return rankMeta.category[this.category];
+    },
+}));
+export function filterRanksByCommand(ranks, command) {
     return ranks.filter((rank) => {
         return rank.commands.filter((_command) => _command === command).length > 0;
     });
 }
-function filterRanksByCategory(ranks, rankCategory) {
-    return ranks.filter((rank) => rank.category === rankCategory);
+export function filterRanksByCategory(ranks, category) {
+    return ranks.filter((rank) => rank.category === category);
 }
-exports.default = {
-    rankMeta: exports.rankMeta,
-    allRanks: exports.allRanks,
-    filterRanksByElement,
+export default {
+    rankMeta,
+    allRanks,
+    filterRanksByCommand,
     filterRanksByCategory,
 };
